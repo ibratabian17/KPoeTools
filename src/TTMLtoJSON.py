123d13b99342768274e1e7006840b2e8a7dcfb59
ibratabian17 <ibratabian@gmail.com>
2024-06-03 12:26:07 +0800

    Add Apple Music Specific Data

--- src/TTMLtoJSON.py
+++ src/TTMLtoJSON.py
@@ -1,7 +1,3 @@
-# KPoeTools - TTML2JSON
-# https://github.com/ibratabian17/KPoeTools
-# Credit Me if u going to use this tools on non-KPoe Games
-
 import xml.etree.ElementTree as ET
 import json
 import sys
@@ -17,23 +13,31 @@ def parse_ttml(file_path, offset=0):
     }
     timing_mode = root.attrib.get('{http://music.apple.com/lyric-ttml-internal}timing', 'Word')
     if timing_mode == "Line":
-        print('[WARNING] This TTML are not Synced Word-by-Word')
-    
+        print('[WARNING] This TTML is not Synced Word-by-Word')
+
     lyrics = []
-    
+
     for div in root.findall('.//tt:div', namespace):
+        song_part = div.attrib.get('{http://music.apple.com/lyric-ttml-internal}songPart', None)
         for p in div.findall('.//tt:p', namespace):
+            song_key = p.attrib.get('{http://music.apple.com/lyric-ttml-internal}key', None)
+            singer = p.attrib.get('{http://www.w3.org/ns/ttml#metadata}agent', None)
             if timing_mode == "Line":
                 p_text = p.text or ""
                 p_begin = time_to_ms(p.attrib['begin'])
                 p_end = time_to_ms(p.attrib['end'])
                 p_duration = p_end - p_begin
-                
+
                 lyrics.append({
                     "time": p_begin + offset,
                     "duration": p_duration,
                     "text": p_text,
-                    "isLineEnding": 1
+                    "isLineEnding": 1,
+                    "element": {
+                        "key": song_key,
+                        "songPart": song_part,
+                        "singer": singer
+                    }
                 })
             else:
                 spans = p.findall('.//tt:span', namespace)
@@ -44,16 +48,21 @@ def parse_ttml(file_path, offset=0):
                         span_begin = time_to_ms(span.attrib['begin'])
                         span_end = time_to_ms(span.attrib['end'])
                         span_duration = span_end - span_begin
-                    
+
                         text += span_text
                         if isinstance(spans[i].tail, str):
                             text += spans[i].tail
-                    
+
                         lyrics.append({
                             "time": span_begin + offset,
                             "duration": span_duration,
                             "text": text,
-                            "isLineEnding": 1 if i == len(spans) - 1 else 0
+                            "isLineEnding": 1 if i == len(spans) - 1 else 0,
+                            "element": {
+                                "key": song_key,
+                                "songPart": song_part,
+                                "singer": singer
+                            }
                         })
                         text = ""
                     else:
@@ -82,7 +91,6 @@ def time_to_ms(time_str):
     total_ms = hours * 60 * 60 * 1000 + minutes * 60 * 1000 + seconds * 1000 + milliseconds
     return total_ms
 
-
 # Example usage:
 if len(sys.argv) < 2:
     print("Usage: python script_name.py your_json_file.json offset")
@@ -101,6 +109,6 @@ lyrics_json = parse_ttml(file_path, offset)
 input_file = sys.argv[1]
 input_filename = os.path.splitext(os.path.basename(input_file))[0]
 with open(f"output/{input_filename}.json", "w", encoding="utf-8") as f:
-        json.dump(lyrics_json, f, indent=4)
+    json.dump(lyrics_json, f, indent=4)
 
-print(f"JSON Generated. Saved to {output_file_path}")
+print(f"JSON Generated. Saved to output/{input_filename}.json")
